afa17ec499ce71b452855f6dd7279d9ab2c30ddd
Brandon Barksdale <bbark93@gmail.com>
2024-11-22 20:25:05 -0500

    post task endpoint

--- api/task/model.js
+++ api/task/model.js
@@ -2,14 +2,22 @@
 const db = require("../../data/dbConfig.js");
 
 async function getTasks() {
-    const taskRows = await db("tasks as t")
-      .leftJoin("projects as p", "t.project_id", "p.project_id")
-      .select("t.*", "p.project_name", "p.project_description"); 
-    
-    return taskRows;
-  }
+  const taskRows = await db("tasks as t")
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .select("t.*", "p.project_name", "p.project_description");
+
+  return taskRows;
+}
+
+function addTask(task) {
+  return db("tasks")
+    .insert(task)
+    .then(([id]) => {
+      return db("tasks").where("task_id", id).first();
+    });
+}
 
 module.exports = {
   getTasks,
-  // addProjects,
+  addTask,
 };
--- api/task/router.js
+++ api/task/router.js
@@ -6,12 +6,24 @@ router.get("/", (req, res, next) => {
   Task.getTasks()
     .then((tasks) => {
       let num = 0;
-      tasks.map( t => {
+      tasks.map((t) => {
         if (t.task_completed == 0) tasks[num].task_completed = false;
         if (t.task_completed == 1) tasks[num].task_completed = true;
         num++;
-      } )
+      });
       res.json(tasks);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  const newTask = req.body;
+
+  Task.addTask(newTask)
+    .then((newTask) => {
+        if (newTask.task_completed == 0) newTask.task_completed = false;
+        if (newTask.task_completed == 1) newTask.task_completed = true;
+      res.status(201).json(newTask);
     })
     .catch(next);
 });
